eb846bed5115e6513a88a1fc1bac166ff45caa28
vh <vihtori.hanninen@gmail.com>
2024-04-23 22:30:30 +0300

    Palautetaan

--- app/src/main/java/com/example/ht/VehicleFragment.java
+++ app/src/main/java/com/example/ht/VehicleFragment.java
@@ -3,15 +3,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import java.util.List;
 import android.widget.TextView;
 
-// VehicleFragment.java
+
 public class VehicleFragment extends Fragment {
 
     private MunicipalityData municipalityData;
