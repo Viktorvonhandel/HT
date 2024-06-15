2832318712fcf287967b3ae12a88e37a078edadb
vh <vihtori.hanninen@gmail.com>
2024-06-16 00:54:00 +0300

    Paluu-toimii, tabit päivittyy, kaikissa fragmenteissa sisältöä+ homebutton toimii, vehicle->propertytax muutettu, muotoiltu fiksummaksi.

--- app/src/main/java/com/example/ht/BasicFragment.java
+++ app/src/main/java/com/example/ht/BasicFragment.java
@@ -5,17 +5,22 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.squareup.picasso.Picasso;
+
 public class BasicFragment extends Fragment {
     private static final String TAG = "BasicFragment";
 
     private TextView populationTextView;
     private TextView weatherTextView;
+    private TextView temperatureTextView;
+    private ImageView weatherIconImageView;
     private MunicipalityData municipalityData;
 
     public static BasicFragment newInstance() {
@@ -25,7 +30,7 @@ public class BasicFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d(TAG, "onCreate-OLLAAN BASICFRAGMENTISSA PERKELE");
+        Log.d(TAG, "onCreate");
     }
 
     @Override
@@ -34,6 +39,8 @@ public class BasicFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_basic, container, false);
         populationTextView = view.findViewById(R.id.populationTextView);
         weatherTextView = view.findViewById(R.id.weatherTextView);
+        temperatureTextView = view.findViewById(R.id.temperatureTextView);
+        weatherIconImageView = view.findViewById(R.id.weatherIconImageView);
         Log.d(TAG, "onCreateView");
 
         // Tarkistetaan, onko datan asettamista varten
@@ -52,8 +59,9 @@ public class BasicFragment extends Fragment {
 
     public void setMunicipalityData(MunicipalityData municipalityData) {
         this.municipalityData = municipalityData;
-        Log.d(TAG, "Tätä ei kyllä vissiin kutsuta");
-        if (municipalityData != null && populationTextView != null && weatherTextView != null) {
+        Log.d(TAG, "setMunicipalityData");
+
+        if (municipalityData != null && populationTextView != null && weatherTextView != null && temperatureTextView != null && weatherIconImageView != null) {
             PopulationData populationData = municipalityData.getPopulationData();
             WeatherData weatherData = municipalityData.getWeatherData();
 
@@ -61,15 +69,19 @@ public class BasicFragment extends Fragment {
                 String populationText = "Population: " + populationData.getPopulation() +
                         "\nPopulation Change: " + populationData.getPopulationChange() + "%" +
                         "\nJob Self Sufficiency: " + populationData.getJobSelfSufficiency() + "%" +
-                        "\nEmployment Rate: " + populationData.getEmploymentRate() +"%";
+                        "\nEmployment Rate: " + populationData.getEmploymentRate() + "%";
                 populationTextView.setText(populationText);
             }
-//NIINKÖ
+
             if (weatherData != null) {
-                String weatherText = "City: " + weatherData.getCityName() +
-                        "\nTemperature: " + weatherData.getTemperature() +"°C"+
-                        "\nIcon Code: " + weatherData.getIconCode() ;
+                String weatherText = weatherData.getCityName();
                 weatherTextView.setText(weatherText);
+
+                String temperatureText = weatherData.getTemperature() + "°C";
+                temperatureTextView.setText(temperatureText);
+
+                String iconUrl = "https://openweathermap.org/img/wn/" + weatherData.getIconCode() + "@2x.png";
+                Picasso.get().load(iconUrl).into(weatherIconImageView);
             }
         }
     }
